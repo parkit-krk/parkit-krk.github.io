5980bbdffe39edaf066f10af16cb4aa63503345d
mgol <michal.golabek94@gmail.com>
2017-03-19 11:58:00 +0100

    website push

---
+++ dev.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+      scrollOverflow:true,
+      responsiveWidth:768,
+      slidesNavigation: true,
+      controlArrows: false,
+      slidesNavPosition: 'top'
+    });
+});
